adbce936e4fe6cfbc5636d348117af9968a217b2
友海 大貫 <k023g0017@g.neec.ac.jp>
2023-10-11 14:34:48 +0900

    日付の出力

--- PM1_Gittest.cpp
+++ PM1_Gittest.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello Github!\n";
 
     std::cout << "LC1C_08_オオヌキ_ユウ\n";
+
+    std::cout << "2023/10/11\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
